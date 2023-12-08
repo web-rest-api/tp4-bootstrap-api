e2a452d9bfcbd47722429a2eaecd61656c45c38c
lostmart <martinpedraza1979@hmail.com>
2023-12-08 09:43:08 +0100

    callback fn, write html

--- script.js
+++ script.js
@@ -2,14 +2,20 @@ const appSection = document.querySelector(".app")
 
 /*  app inisialilzation  */
 const appInit = () => {
-	fetchData("https://basic-rest-flask.martinpedraza.repl.co/api/books")
+	fetchData(
+		"https://basic-rest-flask.martinpedraza.repl.co/api/books",
+		writeHtml
+	)
 }
 
 /* fetch data   */
-const fetchData = (url) => {
+const fetchData = (url, callback) => {
 	fetch(url)
 		.then((res) => {
 			if (res.ok) {
+				res.json().then((data) => {
+					callback(data)
+				})
 			} else {
 				appSection.innerHTML = `<h2 class='text-danger'>Error fetching data ...</h2>
                     <img src="./offline.gif" alt="offline" >
@@ -25,6 +31,23 @@ const fetchData = (url) => {
 }
 
 /*  write html  */
+const writeHtml = (livres) => {
+	livres.forEach((livre) => {
+		appSection.innerHTML += `
+            <div class="col">
+                <article class="card">                
+                    <img src="${livre.imageUrl}" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">${livre.title}</h5>
+                        <button data-bs-toggle="modal"
+			                    data-bs-target="#bookModal" 
+                                class="btn btn-dark edit">Edit</button>
+                    </div>    
+                </article>
+            </div>
+            `
+	})
+}
 
 /*  handle clicks  */
 
