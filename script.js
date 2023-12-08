68d97066a70e17af3e4a72e92048b56f92ced6a0
lostmart <martinpedraza1979@hmail.com>
2023-12-08 09:22:41 +0100

    events and data fetch

--- script.js
+++ script.js
@@ -1,19 +1,29 @@
 const appSection = document.querySelector(".app")
 
+/* fetch data   */
+
+/*  write html  */
+
+/*  handle clicks  */
+
 fetch("https://basic-rest-flask.martinpedraza.repl.co/api/books")
 	.then((response) => response.json())
-    .then((livres) => {
-        livres.forEach(livre => {
-            appSection.innerHTML += `
+	.then((livres) => {
+		livres.forEach((livre) => {
+			appSection.innerHTML += `
             <div class="col">
                 <article class="card">                
                     <img src="${livre.imageUrl}" class="card-img-top" alt="...">
                     <div class="card-body">
                         <h5 class="card-title">${livre.title}</h5>
-                        <button class="btn btn-dark">Edit</button>
+                        <button data-bs-toggle="modal"
+			                    data-bs-target="#bookModal" 
+                                class="btn btn-dark edit">Edit</button>
                     </div>    
                 </article>
             </div>
             `
-        })
-    })
+		})
+		const editBtnArray = document.querySelectorAll(".edit")
+		console.log(editBtnArray, "select btns ...")
+	})
